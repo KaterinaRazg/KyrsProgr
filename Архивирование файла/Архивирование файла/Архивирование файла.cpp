d7b03561931f58dba06b14d1e7d062031667d414
KaterinaRazg <k.razgulova@list.ru>
2025-05-17 00:30:23 +0500

    Add files via upload

---
+++ Архивирование файла/Архивирование файла/Архивирование файла.cpp
@@ -0,0 +1,206 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <list>
+#include <vector>
+using namespace std;
+
+class Usel 
+{
+public: 
+    int kolvo = 0;
+    char simv = 0;
+    Usel* left,*right;
+
+    Usel() { left = right = NULL; }   //Основной конструктор
+
+    Usel (Usel* L, Usel* R)     //Конструктор, чтобы соединать рядом стоящие узлы в один
+    {
+        left = L;
+        right = R;
+        kolvo = L->kolvo + R->kolvo;
+    }
+};
+
+vector<bool> code;
+map<char, vector<bool> > Spisok;
+
+struct DlaSort                //Структура для сортировки листа по возврастания
+{     
+    bool operator() (Usel* lev, Usel* prav) const
+    {
+        return lev->kolvo < prav->kolvo;
+    }
+};
+
+
+void PrisvaivSimvCod(Usel* Vershina)
+{
+    if (Vershina == NULL)
+        return;
+    if (Vershina->left != NULL)
+    {
+        code.push_back(0);
+        PrisvaivSimvCod(Vershina->left);
+    }
+    if (Vershina->right != NULL) 
+    {
+        code.push_back(1);
+        PrisvaivSimvCod(Vershina->right);
+    }
+    if (Vershina->left == NULL && Vershina->right == NULL)
+        Spisok[Vershina->simv] = code;
+
+    if (!code.empty())
+        code.pop_back();
+}
+
+void PodchSimvol(map<char, int> &KolvoSimv, string filename)              //Подсчет символов в файле
+{
+    ifstream file(filename, ios::binary);
+    char isfila;
+
+    if (!file.is_open())
+        printf("Error, file isn\'t open T_T\n");
+    else
+        if (file.eof())
+            printf("File is empty =(\n");
+        else
+        {
+            while (file.get(isfila)) 
+            {
+                KolvoSimv[isfila]++;
+            }
+        }
+
+    
+    file.close();
+}
+
+void ZamenSimvol(string Filename, string Filenamebin)              //Подсчет символов в файле
+{
+    ifstream file(Filename, ios::binary);
+    ofstream itog(Filenamebin, ios::binary);
+    char isfila;
+
+    file.clear();
+    file.seekg(0, ios_base::beg);
+
+    int count = 0; 
+    char buff = 0;
+    while (file.get(isfila))
+    {
+        vector<bool> code = Spisok[isfila];
+
+        for (bool bit : code) 
+        {
+            buff = buff << 1 | bit;
+            count++;
+            if (count == 8) 
+            {
+                itog.put(buff);
+                count = 0;
+                buff = 0;
+            }
+        }
+    }
+
+    file.close();
+
+    if (count > 0) {
+        buff = buff << (8 - count);
+        itog.put(buff);
+    }
+
+    itog.close();
+}
+
+
+void ObratRash(Usel* Vershina, string Falename) 
+{
+    ifstream F(Falename, ios::in | ios::binary);
+
+    Usel* p = Vershina;
+    int count = 0; 
+    char byte;
+    byte = F.get();
+    while (F.eof())
+    {
+        bool b = byte & (1 << (7 - count));
+        if (b) 
+            p = p->right; 
+        else 
+            p = p->left;
+        if (p->left == NULL && p->right == NULL) 
+        { 
+            cout << p->simv; 
+            p = Vershina; 
+        }
+        count++;
+        if (count == 8) 
+        { 
+            count = 0; 
+            byte = F.get(); 
+        }
+    }
+
+    F.close();
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 4) {
+        printf("Error, not coef");
+        return 1;
+    }
+    
+    string choose = argv[1];
+    string Filename = argv[2];
+    string FilenameBin = argv[3];
+
+    map<char, int> KolvoSimv;
+    PodchSimvol(KolvoSimv, Filename);
+
+    list<Usel*> VseSimv;
+
+    map<char, int>::iterator i;
+    for (i = KolvoSimv.begin(); i != KolvoSimv.end(); ++i)
+    {
+        Usel* nUsel = new Usel();
+        nUsel->simv = i->first;
+        nUsel->kolvo = i->second;
+        VseSimv.push_back(nUsel);
+    }
+
+    while (VseSimv.size() > 1)
+    {
+        VseSimv.sort(DlaSort());
+        Usel* Perv = VseSimv.front();
+        VseSimv.pop_front();
+        Usel* Vtor = VseSimv.front();
+        VseSimv.pop_front();
+
+        Usel* glavn = new Usel(Perv, Vtor);
+        VseSimv.push_back(glavn);
+    }
+
+    Usel* Vershina = VseSimv.front();
+    PrisvaivSimvCod(Vershina);
+
+    if (choose == "e")
+    {
+        ZamenSimvol(Filename,FilenameBin);
+    }
+    else
+        if (choose == "d")
+        {
+            ObratRash(Vershina, FilenameBin);
+        }
+        else
+            printf("Error");
+
+    return 0;
+}
+
+
---
+++ Спискимассивстек/Спискимассивстек/Спискимассивстек.cpp
@@ -0,0 +1,296 @@
+﻿#include <iostream>
+using namespace std;
+
+class Array 
+{
+public:
+    int size = 0;
+    int sizearray = 5;
+    int* massiv = new int[sizearray];
+    Array() {  }
+    ~Array() 
+    {
+        delete[] massiv;
+    }
+
+private:
+    void copy(int V[], int New[], int size)
+    {
+        for (int i = 0; i < size; i++)
+            New[i] = V[i];
+    }
+    
+    void AvailabilitySpace()
+    {
+        if (size > (sizeof(int) * sizearray - 1))
+        {
+            int* newArray = new int[sizearray * 2];
+            copy(massiv, newArray, size);
+            delete[] massiv;
+            massiv = newArray;
+            sizearray = sizearray * 2;
+        }
+    }
+
+public:
+    void ArrayGet(int position)
+    {
+        if (position < size)
+            printf("%d", massiv[position]);
+        else
+            printf("Error\n");
+    }
+
+    void ArrayAdd(int position, int value)
+    {
+        if (position <= size)
+        {
+            AvailabilitySpace();
+            for (int i = size; i > position; i--)
+                massiv[i] = massiv[i - 1];
+            massiv[position] = value;
+            size++;
+        }
+        else
+            printf("Error\n");
+    }
+
+    void ArrayRemove(int position)
+    {
+        if (position < size)
+        {
+            for (int i = position; i < size; i++)
+                massiv[i] = massiv[i + 1];
+            massiv[size] = NULL;
+            size--;
+        }
+        else
+            printf("Error\n");
+    }
+
+    void PrintArray() 
+    {
+        for (int i = 0; i < size; i++)
+            printf("V[%d] = %d\n", i, massiv[i]);
+    }
+};
+
+class Node 
+{
+public:
+    int data;
+    Node* next;
+    Node* previous;
+    Node(int data) 
+    {
+        this->data = data;
+        this->next = this->previous = nullptr;
+    }
+};
+
+class List
+{
+public:
+    Node* head;
+    Node* tail;
+    int size;
+    
+    List() 
+    {
+        this->head= this->tail = nullptr;
+        size = 0;
+    }
+
+    ~List()
+    {
+        while (head != nullptr) 
+        {
+            ListRemove(size);
+            size--;
+        }
+    }
+
+public:
+    Node* ListGet(int position)
+    {
+        if (size <= position)
+            printf("Error\n");
+        else
+        {
+            Node* current = head;
+            while (position > 0)
+            {
+                current = current->next;
+                position--;
+            }
+            return current;
+        }
+    }
+
+    void ListAdd(int data, int position)
+    {
+        if (position == 0)
+        {
+            Node* newNode = new Node(data);
+            newNode->next = head;
+            if (head != nullptr)
+                head->previous = newNode;
+            if (tail == nullptr)
+                tail = newNode;
+            head = newNode;
+            newNode->previous = nullptr;
+        }
+        else
+        {
+            if (position == size)
+            {
+                Node* newNode = new Node(data);
+                newNode->previous = tail;
+                if (tail != nullptr)
+                    tail->next = newNode;
+                if (head == nullptr)
+                    head = newNode;
+                tail = newNode;
+                newNode->next = nullptr;
+            }
+            else
+            {
+                if (size != 0) 
+                {
+                    Node* currentnext = ListGet(position);
+                    Node* currentprev = currentnext->previous;
+
+                    Node* newNode = new Node(data);
+
+                    newNode->next = currentnext;
+                    newNode->previous = currentprev;
+                    currentnext->previous = newNode;
+                    currentprev->next = newNode;
+                }
+            }
+        }
+        size++;
+    }
+
+    void ListRemove(int position)
+    {
+        if (size > 0)
+        {
+            if (position == 1)
+            {
+                Node* current = head;
+                head = head->next;
+                head->previous = nullptr;
+                delete current;
+            }
+            else
+            {
+                if (position == size)
+                {
+                    Node* current = tail;
+                    tail = tail->previous;
+                    tail->next = nullptr;
+                    delete current;
+                }
+                else
+                {
+                    Node* current = ListGet(position);
+                    Node* currentnext = current->next;
+                    Node* currentprev = current->previous;
+                    currentprev->next = currentnext;
+                    currentnext->previous = currentprev;
+                    delete current;
+                }
+            }
+            size--;
+        }
+        else
+            printf("List is empty\n");
+    }
+
+    void PrintList() 
+    {
+        Node* current = head;
+        while (current != nullptr)
+        {
+            printf("%d\n", current->data);
+            current = current->next;
+        }
+    }
+};
+
+
+int main()
+{
+    Array Array;
+    List List;
+
+    int choose = 0;
+    int position = 0;
+    int value = 0;
+
+    Node* current = nullptr;
+
+    char input[20], output[20];
+
+    printf("Choose the action:\n 1. Add the data in Array\n 2. Remove the data of Array\n 3. Print the Array\n 4. Print the value from Array of position\n 5. Add the data in List\n 6. Remove the data of List\n 7. Print the List\n 8. Print the value from List of position\n 0. Close the program\n");
+    scanf_s("%d\n", &choose);
+
+    while (choose != 0)
+    {
+        switch (choose)
+        {
+        case 1:
+            printf("Input value ");
+            scanf_s("%d\n", &value);
+            printf("Input nomber position ");
+            scanf_s("%d\n", &position);
+            Array.ArrayAdd(position, value);
+            break;
+        case 2:
+            printf("Input nomber position ");
+            scanf_s("%d\n", &position);
+            Array.ArrayRemove(position);
+            break;
+        case 3:
+            Array.PrintArray();
+            break;
+        case 4:
+            printf("Input nomber position ");
+            scanf_s("%d\n", &position);
+            Array.ArrayGet(position);
+            break;
+        case 5:
+            printf("Input value ");
+            scanf_s("%d\n", &value);
+            printf("Input nomber position ");
+            scanf_s("%d\n", &position);
+            List.ListAdd(value, position);
+            break;
+        case 6:
+            printf("Input nomber position ");
+            scanf_s("%d\n", &position);
+            List.ListRemove(position);
+            break;
+        case 7:
+            List.PrintList();
+            break;
+        case 8:
+            printf("Input nomber position ");
+            scanf_s("%d\n", &position);
+            current = List.ListGet(position);
+            printf("%d", current->data);
+            break;
+        default:
+            printf("Error\n");
+        }
+        printf("Choose the action.. ");
+        scanf_s("%d\n", &choose);
+    }
+
+    delete current;
+    Array.~Array();
+    List.~List();
+    return 0;
+}
+
